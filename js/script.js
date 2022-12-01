8e1579e620c3914a286e11ef824198d0fb595c60
SebastianoUrban <sebastiano.urban98@gmail.com>
2022-12-01 16:41:49 +0100

    add bomb and gameover condition

--- js/script.js
+++ js/script.js
@@ -1,41 +1,104 @@
+//default var
+const numCell = 100;
+const numBomb = 16;
+
+
 
 //I take the elements of the DOM
 const gridElem = document.getElementById('grid');
+const scoreElem = document.getElementById('score');
 const playBtn = document.getElementById('btn-play');
-
-
-// fanction for creation div.square with inside p
-function createSquareElem (index) {
-    //creation the p element with inside number of cell
-    const pElem = document.createElement('p');
-    pElem.innerText = index+1;
-    //creation the div.square element and inserting the p element
-    const divElem = document.createElement('div');
-    divElem.classList.add('square');
-    divElem.appendChild(pElem);
-    //add behavior to the cell
-    divElem.addEventListener('click', function () {
-        console.log(index+1);
-        divElem.classList.toggle('clicked');
-        });
-    //return the div.squared element
-    return divElem;
-};
-
-
-
-//function for fill the 10x10 grid with div.squere
-function fillGrid(gridElem) {
-    gridElem.innerHTML = '';
-    for (let i=0; i<100; i++) {
-        squareElem = createSquareElem(i);
-        gridElem.appendChild(squareElem);
-    }
-};
 
 
 
 //When play bustton clicked 
 playBtn.addEventListener('click', function(){
-    fillGrid(gridElem);
+    
+    const bombList = genBombList(numBomb, 0, numCell-1);
+
+    fillGrid(gridElem, scoreElem, bombList);
 });
+
+
+
+
+
+
+
+
+  //======================================================================= //
+ //============================== fUNCTIONS  ============================= //
+//======================================================================= //
+
+
+//function for fill the 10x10 grid with div.squere
+function fillGrid(gridElem, scoreElem, bombList) {
+
+    gridElem.innerHTML = '';
+    let gameOver = false;
+    let score = 0;
+    scoreElem.innerHTML = score;
+    
+
+
+    for (let i=0; i<100; i++) {
+        //creation the p element with inside number of cell
+        const pElem = document.createElement('p');
+        //pElem.innerText = i+1;
+
+        //creation the div.square element and inserting the p element
+        const divElem = document.createElement('div');
+        divElem.classList.add('square');
+        divElem.appendChild(pElem);
+
+        //add behavior to the cell
+        let clicked = false;
+
+        divElem.addEventListener('click', function () {
+            if (!gameOver && !clicked) {
+                clicked = true;
+                if (bombList.includes(i)) {
+                    divElem.classList.add('red-bomb');
+                    gameOver = true;
+                } else {
+                    divElem.classList.add('clicked');
+                    score++;
+                    scoreElem.innerHTML = score;
+                }
+            }
+
+            if (!gameOver && (score == 84) ) {
+                gameOver = true;
+                alert('HAI VINTO!!!!');
+            }
+            //console.log(score);
+        });
+
+        // divElem.addEventListener('contextmenu', function (e) {
+        //     e.preventDefault();
+        //     divElem.classList.toggle('flag');
+        // });
+        gridElem.appendChild(divElem);
+    }
+};
+
+
+//function random number in range
+function randomNumberRange(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1) + min);
+    return num;
+}
+
+
+//generation 'nBomb' list from 'min' index and 'max index
+function genBombList (nBomb, min, max) {
+    const outputBombList = [];
+    while (outputBombList.length < nBomb) {
+        const randomNumber = randomNumberRange(min, max);
+        if ( !(outputBombList.includes(randomNumber))  ) {
+            outputBombList.push(randomNumber);
+        }
+    }
+    console.log(outputBombList);
+    return outputBombList;
+};
